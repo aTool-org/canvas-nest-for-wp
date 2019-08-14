dbe6fb63cfc4194dfb7d9f44fbce33d30cdc16e6
Hans <tyhan@bupt.edu.cn>
2019-08-14 19:58:49 +0800

    Fix the bug of Settings
    Add Argument  of do_settings_fields()

--- templates/settings.php
+++ templates/settings.php
@@ -2,7 +2,7 @@
     <h2>Canvas-nest.js Setting</h2>
     <form method="post" action="options.php"> 
         <?php @settings_fields('WP_Canvas_Nest-group'); ?>
-        <?php @do_settings_fields('WP_Canvas_Nest-group'); ?>
+        <?php @do_settings_fields('WP_Canvas_Nest-group', None); ?>
 
         <?php do_settings_sections('WP_Canvas_Nest'); ?>
         <?php @submit_button(); ?>
@@ -10,7 +10,7 @@
     <hr />
     <form method="post" action="options.php"> 
         <?php @settings_fields('WP_Canvas_Nest-checkbox-group'); ?>
-        <?php @do_settings_fields('WP_Canvas_Nest-checkbox-group'); ?>
+        <?php @do_settings_fields('WP_Canvas_Nest-checkbox-group', None); ?>
 
         <?php do_settings_sections('WP_Canvas_Nest_Checkbox'); ?>
         <?php @submit_button(); ?>
